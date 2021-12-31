7e0f1aa916aab383db37b5e4aa1b79a87f98e065
aasgoas <95178274+aasgoas@users.noreply.github.com>
2021-12-31 22:26:58 +0700

    Create MenuButtons.js

---
+++ MenuButtons.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import FontAwesame from "react-native-vector-icons/FontAwesome";
+
+const items = [
+    { id: 1, name: "video-camera", title : "New Meeting", customColor: "#FF751F"},
+    { id: 2, name: "plus-square", title : "Join"},
+    { id: 3, name: "calendar", title : "Schedule"},
+    { id: 4, name: "upload", title : "Share Screen"},
+]
+
+
+
+function MenuButtons({navigation}) {
+
+    const openMeeting = () => {
+        navigation.navigate("Room")
+    }
+
+    return (
+        <View style={styles.container}>
+            {items.map(item => (
+            <View
+                key={item.id}
+                style={styles.buttonContainer}>
+                <TouchableOpacity
+                    onPress={()=>openMeeting()}
+                    style={{
+                        ...styles.button,
+                        backgroundColor: item.customColor ? item.customColor : "#0470DC"
+                    }}
+                >
+                    <FontAwesame name={item.name} size={23} color={"#efefef"} />   
+                </TouchableOpacity>
+                <Text style={styles.menuText} key={item.id}>{item.title}</Text>
+            </View>
+            ) ) }
+        </View>
+    )
+}
+
+export default MenuButtons
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 25,
+        paddingBottom: 10,
+        borderBottomColor: "#1F1F1F",
+        borderBottomWidth: 1,
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    buttonContainer: {
+        alignItems: "center",
+        flex: 1
+
+    },
+    button: {
+        width: 50,
+        height: 50,
+        borderRadius: 15,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    menuText: {
+        color: "#858585",
+        fontSize: 12,
+        paddingTop: 10,
+        fontWeight: "600"
+    }
+})
